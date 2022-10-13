7ef27751710566db8d67aa4ff4ed678f49f16bad
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-13 17:17:52 +0200

    Set up main window.

--- WPF MVVM Application/App.xaml.cs
+++ WPF MVVM Application/App.xaml.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using WPF_MVVM_Application.ViewModels;
 
 namespace WPF_MVVM_Application;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application {
+
+    public App() {
+
+    }
+
+    protected override void OnStartup(StartupEventArgs e) {
+
+        MainWindow = new MainWindow();
+        MainWindow.DataContext = new MainViewModel(new(), MainWindow);
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
 }
---
+++ WPF MVVM Application/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+using System.Windows;
+using System.Windows.Input;
+using WPF_MVVM_Application.Commands.UI;
+using WPF_MVVM_Application.Stores;
+using WPF_MVVM_Application.ViewModels.Bases;
+
+namespace WPF_MVVM_Application.ViewModels;
+
+/// <summary>
+/// The main viewmodel of the application. Stores data on the current view model and commands to interact with the window.
+/// </summary>
+public class MainViewModel : ViewModelBase {
+
+    private readonly NavigationStore _navigationStore;
+    public ViewModelBase? CurrentViewModel => _navigationStore.CurrentViewModel;
+
+    public ICommand MinimizeCommand { get; }
+    public ICommand ResizeCommand { get; }
+    public ICommand CloseCommand { get; }
+
+    public MainViewModel(NavigationStore navigationStore, Window mainWindow) {
+        _navigationStore = navigationStore;
+        MinimizeCommand = new MinimizeCommand(mainWindow);
+        ResizeCommand = new ResizeCommand(mainWindow);
+        CloseCommand = new CloseCommand(mainWindow);
+
+        _navigationStore.CurrentViewModelChanged += OnCurrentViewChanged;
+    }
+
+    private void OnCurrentViewChanged() {
+        OnPropertyChanged(nameof(CurrentViewModel));
+    }
+
+}
