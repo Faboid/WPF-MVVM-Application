aae5cb5691130ce314a5ed1e504eedbf7ce29572
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-13 16:30:59 +0200

    Set up base viewmodels.

---
+++ WPF MVVM Application/ViewModels/Bases/ErrorsViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WPF_MVVM_Application.ViewModels.Bases;
+
+public class ErrorsViewModel : INotifyDataErrorInfo
+{
+
+    private readonly Dictionary<string, List<string>> _propertyErrors = new();
+    public bool HasErrors => _propertyErrors.Any();
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    public IEnumerable GetErrors(string? propertyName)
+    {
+        if (propertyName != null)
+        {
+            return _propertyErrors.GetValueOrDefault(propertyName, new());
+        }
+
+        return Array.Empty<string>();
+    }
+
+    public void AddError(string propertyName, string errorMessage)
+    {
+        if (!_propertyErrors.ContainsKey(propertyName))
+        {
+            _propertyErrors.Add(propertyName, new());
+        }
+
+        _propertyErrors[propertyName].Add(errorMessage);
+        OnErrorsChanged(propertyName);
+    }
+
+    public void ClearErrors(string propertyName)
+    {
+        if (_propertyErrors.Remove(propertyName))
+        {
+            OnErrorsChanged(propertyName);
+        }
+    }
+
+    private void OnErrorsChanged(string propertyName)
+    {
+        ErrorsChanged?.Invoke(this, new(propertyName));
+    }
+
+}
---
+++ WPF MVVM Application/ViewModels/Bases/ViewModelBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WPF_MVVM_Application.ViewModels.Bases;
+public class ViewModelBase : INotifyPropertyChanged, IDisposable
+{
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged(string? propertyName)
+    {
+        PropertyChanged?.Invoke(this, new(propertyName));
+    }
+
+    protected void SetAndRaise<T>(string name, ref T prop, T value)
+    {
+        prop = value;
+        OnPropertyChanged(name);
+    }
+
+    private bool _isDisposed = false;
+    public void Dispose()
+    {
+        if (!_isDisposed)
+        {
+            Dispose(_isDisposed);
+        }
+        _isDisposed = true;
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposed) { }
+
+}
