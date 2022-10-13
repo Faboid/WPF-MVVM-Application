8347f7fa40ffe7e491f4962b9b143f44c16a5822
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-13 16:31:26 +0200

    Set up base command.

---
+++ WPF MVVM Application/Commands/Bases/CommandBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Input;
+
+namespace WPF_MVVM_Application.Commands.Bases;
+
+/// <summary>
+/// Represents a generic command's framework.
+/// </summary>
+public abstract class CommandBase : ICommand, IDisposable {
+
+    public event EventHandler? CanExecuteChanged;
+
+    public virtual bool CanExecute(object? parameter) {
+        return true;
+    }
+
+    public abstract void Execute(object? parameter);
+
+    protected void OnCanExecuteChanged() {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    //0 == false
+    //1 == true
+    private int _isDisposed = 0;
+    protected virtual void Dispose(bool disposing) { }
+    public void Dispose() {
+        if(Interlocked.Exchange(ref _isDisposed, 1) == 0) {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+}
