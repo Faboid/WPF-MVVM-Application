0bdcbeb0881a279e722a768d573a1a15fac35ef1
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-13 17:21:27 +0200

    Set up navigation commands.

---
+++ WPF MVVM Application/Commands/UI/AsyncNavigateCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using WPF_MVVM_Application.Commands.Bases;
+using WPF_MVVM_Application.Services;
+using WPF_MVVM_Application.ViewModels.Bases;
+
+namespace WPF_MVVM_Application.Commands.UI;
+
+/// <summary>
+/// Provides a method to generate an argument asynchonously to be passed to a new generated view. The current view will shift to <see cref="TViewModel"/> when executed.
+/// </summary>
+/// <typeparam name="TViewModel">The viewmodel to navigate to.</typeparam>
+/// <typeparam name="TArgument">The argument to be passed to the viewmodel.</typeparam>
+public class AsyncNavigateCommand<TViewModel, TArgument> : AsyncCommandBase where TViewModel : ViewModelBase
+{
+
+    private readonly Func<Task<TArgument>> _getArgumentAsync;
+    private readonly NavigationService<TViewModel, TArgument> _navigationService;
+    private readonly bool _disposeCurrent;
+
+    public AsyncNavigateCommand(Func<Task<TArgument>> lazyArgumentAsync, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgumentAsync = lazyArgumentAsync;
+    }
+
+    public AsyncNavigateCommand(Func<Task<TArgument>> lazyArgumentAsync, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService, BusyService busyService) : base(busyService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgumentAsync = lazyArgumentAsync;
+    }
+
+    protected override async Task ExecuteAsync(object? parameter)
+    {
+        var argument = await _getArgumentAsync.Invoke();
+        _navigationService.Navigate(argument, _disposeCurrent);
+    }
+}
---
+++ WPF MVVM Application/Commands/UI/NavigateCommand.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using WPF_MVVM_Application.Commands.Bases;
+using WPF_MVVM_Application.Services;
+using WPF_MVVM_Application.ViewModels.Bases;
+
+namespace WPF_MVVM_Application.Commands.UI;
+
+/// <summary>
+/// Used to navigate between <see cref="ViewModelBase"/>.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class NavigateCommand<T> : LinkableCommandBase where T : ViewModelBase {
+
+    private readonly NavigationService<T> _navigationService;
+    private readonly bool _disposeCurrent;
+
+    public NavigateCommand(bool disposeCurrent, NavigationService<T> navigationService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+    }
+
+    public NavigateCommand(bool disposeCurrent, NavigationService<T> navigationService, BusyService busyService) : base(busyService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+    }
+
+    public override void ExecuteLinked(object? parameter)
+    {
+        _navigationService.Navigate(_disposeCurrent);
+    }
+}
+
+/// <summary>
+/// Used to navigate between <see cref="ViewModelBase"/> by giving an argument.
+/// </summary>
+/// <typeparam name="TViewModel">The viewmodel to navigate to.</typeparam>
+/// <typeparam name="TArgument">The type of argument to give to the new viewmodel.</typeparam>
+public class NavigateCommand<TViewModel, TArgument> : LinkableCommandBase where TViewModel : ViewModelBase {
+
+    private readonly Func<TArgument> _getArgument;
+    private readonly NavigationService<TViewModel, TArgument> _navigationService;
+    private readonly bool _disposeCurrent;
+
+    public NavigateCommand(Func<TArgument> lazyArgument, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgument = lazyArgument;
+    }
+
+    public NavigateCommand(Func<TArgument> lazyArgument, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService, BusyService busyService) : base(busyService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgument = lazyArgument;
+    }
+
+    public NavigateCommand(TArgument argument, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgument = () => argument;
+    }
+
+    public NavigateCommand(TArgument argument, bool disposeCurrent, NavigationService<TViewModel, TArgument> navigationService, BusyService busyService) : base(busyService)
+    {
+        _navigationService = navigationService;
+        _disposeCurrent = disposeCurrent;
+        _getArgument = () => argument;
+    }
+
+    public override void ExecuteLinked(object? parameter)
+    {
+        _navigationService.Navigate(_getArgument.Invoke(), _disposeCurrent);
+    }
+}
