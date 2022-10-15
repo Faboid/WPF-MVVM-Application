686ae67520c6a45154174089a7f6a99930d815cc
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-15 10:55:27 +0200

    Extracted the view presenter from the main window to a custom user control.

---
+++ WPF MVVM Application/Components/ViewPresenter.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_MVVM_Application.Components;
+
+/// <summary>
+/// Connects the views and the viewmodels.
+/// </summary>
+public partial class ViewPresenter : UserControl {
+
+    public ViewModelBase CurrentViewModel {
+        get { return (ViewModelBase)GetValue(CurrentViewModelProperty); }
+        set { SetValue(CurrentViewModelProperty, value); }
+    }
+
+    public static readonly DependencyProperty CurrentViewModelProperty =
+        DependencyProperty.Register("CurrentViewModel", typeof(ViewModelBase), typeof(ViewPresenter), new PropertyMetadata(null));
+
+    public ViewPresenter() {
+        InitializeComponent();
+    }
+}
