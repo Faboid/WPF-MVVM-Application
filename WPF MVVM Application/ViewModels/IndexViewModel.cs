927365552d66bbb85f0476ba817d28710ade4420
Faboid <72757782+Faboid@users.noreply.github.com>
2022-10-17 11:10:04 +0200

    Provided example on the usage of ErrorsViewModel.

--- WPF MVVM Application/ViewModels/IndexViewModel.cs
+++ WPF MVVM Application/ViewModels/IndexViewModel.cs
@@ -1,14 +1,71 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
 using System.Windows.Input;
 using WPF_MVVM_Application.Services;
 
 namespace WPF_MVVM_Application.ViewModels;
 
-public class IndexViewModel : ViewModelBase {
+/// <summary>
+/// An index VM to provide an example of the usage of <see cref="ViewModelBase"/> and <see cref="ErrorsViewModel"/>.
+/// </summary>
+public class IndexViewModel : ViewModelBase, INotifyDataErrorInfo {
 
-    public ICommand HelloCommand { get; }
+	private readonly INotificationService _notificationService;
+	private readonly ErrorsViewModel _errorsViewModel = new();
 
-    public IndexViewModel(INotificationService notificationService) {
-        HelloCommand = new RelayCommand(() => notificationService.Send("Hello!"));
+	private string _name = "";
+	public string Name {
+		get { return _name; }
+		set { SetAndRaise(nameof(Name), ref _name, value); }
+	}
+
+	private int _age;
+	public int Age {
+		get { return _age; }
+		set { 
+			SetAndRaise(nameof(Age), ref _age, value);
+			_errorsViewModel.ClearErrors(nameof(Age));
+			if(value < 13) {
+				_errorsViewModel.AddError(nameof(Age), "You have to be at least 13 years old.");
+			}
+		}
+	}
+
+	public ICommand HelloCommand { get; }
+
+    private void Greet() {
+        _notificationService.Send($"Hello, {Name}!");
     }
 
+	public IndexViewModel(INotificationService notificationService) {
+		_notificationService = notificationService;
+        HelloCommand = new RelayCommand(Greet);
+		_errorsViewModel.ErrorsChanged += OnErrorsChanged;
+		PropertyChanged += OnCanConfirmChanged;
+    }
+
+	public bool CanConfirm => !string.IsNullOrWhiteSpace(Name) && !HasErrors;
+	private void OnCanConfirmChanged(object? sender, PropertyChangedEventArgs e) {
+		//to avoid stackoverflow
+		if(e.PropertyName != nameof(CanConfirm)) {
+			OnPropertyChanged(nameof(CanConfirm));
+		}
+	}
+
+	public bool HasErrors => _errorsViewModel.HasErrors;
+    public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+    private void OnErrorsChanged(object? sender, DataErrorsChangedEventArgs e) {
+        ErrorsChanged?.Invoke(this, e);
+    }
+
+    public IEnumerable GetErrors(string? propertyName) => _errorsViewModel.GetErrors(propertyName);
+
+	protected override void Dispose(bool disposed) {
+		_errorsViewModel.ErrorsChanged -= OnErrorsChanged;
+		PropertyChanged -= OnCanConfirmChanged;
+		
+		base.Dispose(disposed);
+	}
 }
